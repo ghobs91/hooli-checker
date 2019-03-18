ec89ae86caf66005ae6ed239de84f11a6acba88b
Andrew <ghobrial.andrew@gmail.com>
2019-03-18 16:39:04 -0400

    added high level comments to map out my logic

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const parse = require('csv-parse/lib/sync'); // needed for parsing CSV file data
 
 function linkBuyerToFacility() {
   // your solution goes here
+
+  // read sam-accounts.csv
+
+  // read existing-accounts.csv
+
+  // write efficient lookup that doesn't require all of existing-accounts.csv to be read for every single entry in sam-accounts.csv (that would be incredibly slow at scale)
+
+  // for every sam-accounts.csv entry that doesn't appear in existing-accounts.csv, add it to set-differences.csv
+  
 }
 
 linkBuyerToFacility();
