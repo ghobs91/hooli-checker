1ee4beddabe3891529f82c2d91f879114adea282
Andrew <ghobrial.andrew@gmail.com>
2019-03-18 20:31:24 -0400

    timestamp console logs show the function runs in under 2 seconds

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const parse = require('csv-parse/lib/sync'); // needed for parsing CSV file data
 function linkBuyerToFacility() {
   // your solution goes here
   // 
+  var ts = new Date();
+  console.log("function has begun at" + ts.toISOString())
 
   // read sam-accounts.csv
   fs.readFile('sam-accounts.csv', (err, samAccountsFile) => {
@@ -13,46 +15,39 @@ function linkBuyerToFacility() {
       const samAccountsFileLines = parse(samAccountsFile, {
         columns: true
       })
-
       const existingAccountsFileLines = parse(existingAccountsFile, {
         columns: true
       })
+
+      var ts1 = new Date();
+      console.log("parsed files at" + ts1.toISOString())
 
       // write efficient lookup that doesn't require all of existing-accounts.csv to be repeatedly read for every single entry in sam-accounts.csv (that would be incredibly slow at scale)
       var existingAccountsFileLookup = {};
       existingAccountsFileLines.forEach(function (existingLine){
         // ignore last 3 characters in existing-accounts
-        // console.log("existingLine is:" + existingLine)
-        // console.log("existingLine hooli ID is:" + existingLine["hooliId"])
-
         existingHooliID = existingLine["hooliId"].slice(0, -3);
-        console.log("existingHooliID is:" + existingHooliID)
-
         existingAccountsFileLookup[existingHooliID] = true;
-        // console.log("existingAccountsFileLookup[existingLine] we just created is:" + existingLine)    
       });
+
+      var ts2 = new Date();
+      console.log("populated lookup dict at " + ts2.toISOString())
       
        // for every sam-accounts.csv entry that doesn't appear in existing-accounts.csv, add it to set-differences.csv
       samAccountsFileLines.forEach(function(samAccountsLine){
-        samAccountsLine = samAccountsLine["accountHooliId"]
-        // console.log("samAccountsLine is:" + samAccountsLine)
-        // console.log("existingAccountsFileLookup[samAccountsLine] is:" + existingAccountsFileLookup[samAccountsLine])
-        if(existingAccountsFileLookup[samAccountsLine] != true){
-          fs.appendFileSync('set-differences.csv', samAccountsLine + '\n');
+        if(existingAccountsFileLookup[samAccountsLine["accountHooliId"]] != true){
+          fs.appendFileSync('set-differences.csv', samAccountsLine["accountHooliId"] + '\n');
         }
       })
+
+      var ts3 = new Date();
+      console.log("completed function at " + ts3.toISOString())
 
     })
 
   })
   
-
-  
-
-  
-
  
-  
 }
 
 linkBuyerToFacility();
