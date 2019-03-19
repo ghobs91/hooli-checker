51aeb5aec8e4cc62e78cd3416a1a0ef865877209
Andrew <ghobrial.andrew@gmail.com>
2019-03-18 20:41:35 -0400

    improved variable naming

--- index.js
+++ index.js
@@ -12,31 +12,32 @@ function linkBuyerToFacility() {
     // read existing-accounts.csv
     fs.readFile('existing-accounts.csv', (err, existingAccountsFile) => {
       
-      const samAccountsFileLines = parse(samAccountsFile, {
+      const samAccountsParsed = parse(samAccountsFile, {
         columns: true
       })
-      const existingAccountsFileLines = parse(existingAccountsFile, {
+      const existingAccountsParsed = parse(existingAccountsFile, {
         columns: true
       })
 
       var ts1 = new Date();
       console.log("parsed files at" + ts1.toISOString())
 
-      // write efficient lookup that doesn't require all of existing-accounts.csv to be repeatedly read for every single entry in sam-accounts.csv (that would be incredibly slow at scale)
-      var existingAccountsFileLookup = {};
-      existingAccountsFileLines.forEach(function (existingLine){
+      // write efficient dictionary lookup that doesn't require all of existing-accounts.csv to be repeatedly read for every single entry in sam-accounts.csv (that would be incredibly slow at scale)
+      var existingAccountsLookup = {};
+      existingAccountsParsed.forEach(function (existingEntry){
         // ignore last 3 characters in existing-accounts
-        existingHooliID = existingLine["hooliId"].slice(0, -3);
-        existingAccountsFileLookup[existingHooliID] = true;
+        var existingHooliID = existingEntry["hooliId"].slice(0, -3);
+        // create entry in lookup dict with respective HooliID
+        existingAccountsLookup[existingHooliID] = true;
       });
 
       var ts2 = new Date();
       console.log("populated lookup dict at " + ts2.toISOString())
       
        // for every sam-accounts.csv entry that doesn't appear in existing-accounts.csv, add it to set-differences.csv
-      samAccountsFileLines.forEach(function(samAccountsLine){
-        if(existingAccountsFileLookup[samAccountsLine["accountHooliId"]] != true){
-          fs.appendFileSync('set-differences.csv', samAccountsLine["accountHooliId"] + '\n');
+      samAccountsParsed.forEach(function(samAccountsEntry){
+        if(existingAccountsLookup[samAccountsEntry["accountHooliId"]] != true){
+          fs.appendFileSync('set-differences.csv', samAccountsEntry["accountHooliId"] + '\n');
         }
       })
 
@@ -44,10 +45,7 @@ function linkBuyerToFacility() {
       console.log("completed function at " + ts3.toISOString())
 
     })
-
-  })
-  
- 
+  }) 
 }
 
 linkBuyerToFacility();
